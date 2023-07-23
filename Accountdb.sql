706c01cd2811206bc28c5316f970988dbb6a8570
castmaker2 <castmaker8054@gmail.com>
2023-07-23 17:59:36 +0800

    implemented basic signup/login function on header

--- Accountdb.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE Accountdb;
-
-USE Accountdb;
-
-CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(100) NOT NULL UNIQUE,
-  password VARCHAR(100) NOT NULL
-);
